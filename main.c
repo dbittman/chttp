76734d7658f858b85695cc3a4e719e062f126e1b
Daniel Bittman <danielbittman1@gmail.com>
2016-08-28 16:11:43 -0700

    Added mime database.

--- main.c
+++ main.c
@@ -14,10 +14,81 @@
 #include <sys/syscall.h>
 #include <fcntl.h>
 
-static char *hashtable[4096] = {NULL};
+struct entry {
+	char *key;
+	char *val;
+	size_t vallen;
+};
+
+struct entry *hashtable;
+size_t hashtablelen = 512;
+size_t num_entries = 0;
+
+void ingest(struct entry *table, size_t tablen, char *ext, char *mime);
+/* http://cseweb.ucsd.edu/~kube/cls/100/Lectures/lec16/lec16-16.html */
+size_t hashfn(const char *key, size_t tablen)
+{
+	long hashVal = 0;
+	while (*key != '\0') {
+		hashVal = (hashVal << 4) + *(key++);
+		long g = hashVal & 0xF0000000L;
+		if (g != 0) hashVal ^= g >> 24;
+		hashVal &= ~g;
+	}
+	return hashVal % tablen;
+}
+
+void grow_hashtable(void)
+{
+	size_t newlen = hashtablelen * 2;
+	struct entry *newtable = calloc(newlen, sizeof(struct entry));
+	for(size_t i=0;i<hashtablelen;i++) {
+		struct entry *ent = &hashtable[i];
+		if(ent->key != NULL) {
+			ingest(newtable, newlen, ent->key, ent->val);
+			free(ent->key);
+			free(ent->val);
+		}
+	}
+	free(hashtable);
+	hashtable = newtable;
+	hashtablelen = newlen;
+}
+
+char *lookup(const char *key, size_t *len)
+{
+	size_t index = hashfn(key, hashtablelen);
+	for(size_t i=0;i<hashtablelen;i++) {
+		struct entry *ent = &hashtable[(index + i) % hashtablelen];
+		if(ent->key && !strcmp(ent->key, key)) {
+			*len = ent->vallen;
+			return ent->val;
+		}
+	}
+	return NULL;
+}
+
+/* TODO: better hashing */
+void ingest(struct entry *table, size_t tablen, char *ext, char *mime)
+{
+	size_t index = hashfn(ext, tablen);
+	for(size_t i=0;i<tablen;i++) {
+		struct entry *ent = &table[(index + i) % tablen];
+
+		if(ent->key == NULL) {
+			ent->key = strdup(ext);
+			ent->val = strdup(mime);
+			ent->vallen = strlen(ent->val);
+			return;
+		}
+	}
+	fprintf(stderr, "Mime type hash table too small.\n");
+	abort();
+}
 
 void init_mime_database(void)
 {
+	hashtable = calloc(hashtablelen, sizeof(struct entry));
 	FILE *f = fopen("/etc/mime.types", "r");
 	if(f == NULL) {
 		perror("Failed to open mime database");
@@ -29,25 +100,18 @@ void init_mime_database(void)
 		if(buffer[0] == '#' || buffer[0] == '\n')
 			continue;
 
-		char *tok = strtok(buffer, " \t");
-		printf(":: %s\n", tok);
-		tok = strtok(NULL, " \t");
-		printf(":: %s\n", tok);
+		/* some mime types have multiple extensions */
+		char *mime = strtok(buffer, " \t");
+		char *extension;
+		while((extension = strtok(NULL, " \t\n"))) {
+			ingest(hashtable, hashtablelen, extension, mime);
+			if((num_entries++ * 100) / hashtablelen > 25) {
+				grow_hashtable();
+			}
+		}
 	}
+	printf("mime database initialized with %ld entries\n", num_entries);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 char *server_root = "root";
 int num_threads = 128;
@@ -69,12 +133,14 @@ static _Atomic long last_mailbox = 0;
 
 void write_content_type(int client, char *path)
 {
-	write(client, "Content-Type: text/", 19);
+	write(client, "Content-Type: ", 14);
 	char *last_dot = strrchr(path, '.');
 	if(last_dot) {
-		write(client, last_dot + 1, strlen(last_dot + 1));
+		size_t len;
+		char *m = lookup(last_dot + 1, &len);
+		write(client, m, len);
 	} else {
-		write(client, "plain", 5);
+		write(client, "text/plain", 10);
 	}
 	write(client, "\r\n", 2);
 }
